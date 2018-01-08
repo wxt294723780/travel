3d1bea10e04c87a6cd4d9929dda4c8d68e226028
wxt <172002236@qq.com>
2018-01-08 22:51:03 +0800

    前台首页

---
+++ yii/frontend/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+class IndexController extends Controller{
+    public $layout=false;
+    public function actionIndex(){
+       return  $this->render('index');
+    }
+}
